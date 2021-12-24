a2972b664d7dde93334f61f82947e7bfe22377f9
joohyun <joo6058@skku.edu>
2021-12-24 15:39:19 +0900

    scratch.py, main3_pcd.py 업로드
    scratch.py = bin -> pcd로 저장하는 코드
    main3_pcd.py = pcd불러와서 돌리는 main 코드

---
+++ lidar_code/scratch.py
@@ -0,0 +1,58 @@
+import open3d as o3d
+import numpy as np
+import matplotlib.pyplot as plt
+
+# R = o3d.geometry.get_rotation_matrix_from_xyz([0, 0, np.pi/2])
+# print(o3d.geometry.get_rotation_matrix_from_xyz([0, 0, np.pi/2]))
+
+# a = [1, 2]
+# as_a = np.asarray(a)
+# print(as_a)
+# as_aa = np.asarray([1,2])
+# print(as_aa)
+
+# colors = plt.get_cmap("tab20")([1,1,1])
+# plt.show(colors)
+
+# a = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+# print(a[:,0])
+
+import open3d as o3d
+import numpy as np
+import struct
+import os
+import sys
+
+# training 0005
+
+list_pcd = []
+size_float = 4
+print(os.getcwd() + '/lidar_data')
+file_list = sorted(os.listdir(os.getcwd() + '/lidar_data/'))  # sorted(os.listdir('/home/amlab/SemanticKITTI_point/dataset/sequences_0.06/' + scenario_num + '/velodyne/'))
+# os.chdir(os.getcwd() + '/lidar_data/')  # os.chdir('/home/amlab/SemanticKITTI_point/dataset/sequences_0.06/' + scenario_num + '/velodyne/') # change dir
+
+for i in range(len(file_list)):
+    with open (os.getcwd() + f"/lidar_data/{file_list[i]}", "rb") as f:
+        byte = f.read(size_float*4)
+        while byte:
+            x,y,z,intensity = struct.unpack("ffff", byte)
+            print(f"x = {x}, y = {y}, z = {z}")
+            list_pcd.append([x, y, z])
+            byte = f.read(size_float*4)
+    np_pcd = np.asarray(list_pcd)
+    pcd = o3d.geometry.PointCloud()
+    v3d = o3d.utility.Vector3dVector
+    pcd.points = v3d(np_pcd)
+    o3d.io.write_point_cloud(os.getcwd() + f"/lidar_data_pcd/{i}.pcd", pcd)
+
+
+# # Load binary point cloud
+# bin_pcd = np.fromfile("lidar_velodyne64.bin", dtype=np.float32)
+
+# # Reshape and drop reflection values
+# points = bin_pcd.reshape((-1, 4))[:, 0:3]
+
+# # Convert to Open3D point cloud
+# o3d_pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(points))
+
+# # Save to whatever format you like

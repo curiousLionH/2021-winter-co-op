52e2a74c41300c2cbf4b8d0540015955c103aecf
joohyun <joo6058@skku.edu>
2021-12-28 13:56:01 +0900

    convex hull

--- lidar_code/open3d_test.py
+++ lidar_code/open3d_test.py
@@ -1,6 +1,74 @@
 import time
 import open3d as o3d
 import numpy as np
+from scipy.spatial import ConvexHull
+
+def minimum_bounding_rectangle(points):
+    """
+    Find the smallest bounding rectangle for a set of points.
+    Returns a set of points representing the corners of the bounding box.
+
+    :param points: an nx2 matrix of coordinates
+    :rval: an nx2 matrix of coordinates
+    """
+    from scipy.ndimage.interpolation import rotate
+    pi2 = np.pi/2.
+
+    # get the convex hull for the points
+    hull_points = points[ConvexHull(points).vertices]
+
+    # calculate edge angles
+    edges = np.zeros((len(hull_points)-1, 2))
+    edges = hull_points[1:] - hull_points[:-1]
+
+    angles = np.zeros((len(edges)))
+    angles = np.arctan2(edges[:, 1], edges[:, 0])
+
+    angles = np.abs(np.mod(angles, pi2))
+    angles = np.unique(angles)
+
+    # find rotation matrices
+    # XXX both work
+    rotations = np.vstack([
+        np.cos(angles),
+        np.cos(angles-pi2),
+        np.cos(angles+pi2),
+        np.cos(angles)]).T
+#     rotations = np.vstack([
+#         np.cos(angles),
+#         -np.sin(angles),
+#         np.sin(angles),
+#         np.cos(angles)]).T
+    rotations = rotations.reshape((-1, 2, 2))
+
+    # apply rotations to the hull
+    rot_points = np.dot(rotations, hull_points.T)
+
+    # find the bounding points
+    min_x = np.nanmin(rot_points[:, 0], axis=1)
+    max_x = np.nanmax(rot_points[:, 0], axis=1)
+    min_y = np.nanmin(rot_points[:, 1], axis=1)
+    max_y = np.nanmax(rot_points[:, 1], axis=1)
+
+    # find the box with the best area
+    areas = (max_x - min_x) * (max_y - min_y)
+    best_idx = np.argmin(areas)
+
+    # return the best box
+    x1 = max_x[best_idx]
+    x2 = min_x[best_idx]
+    y1 = max_y[best_idx]
+    y2 = min_y[best_idx]
+    r = rotations[best_idx]
+
+    rval = np.zeros((4, 2))
+    rval[0] = np.dot([x1, y2], r)
+    rval[1] = np.dot([x2, y2], r)
+    rval[2] = np.dot([x2, y1], r)
+    rval[3] = np.dot([x1, y1], r)
+
+    return rval
+
 
 def draw_speed_vector(x, y, v_x, v_y):
     if v_x >300:
@@ -75,6 +143,12 @@ def rotation_matrix(roll, pitch, yaw):
 
 def homogeneous_matrix(x, y, z, roll, pitch, yaw):
 
+    '''
+    [x, roll/
+     y, pitch/
+     z, yaw]]
+    '''
+
     R = rotation_matrix(roll, pitch, yaw)
     T = np.array([x, y, z])
     Rt = np.column_stack((R, T))
@@ -82,17 +156,16 @@ def homogeneous_matrix(x, y, z, roll, pitch, yaw):
     
     return Rt
 
-
-def draw_3d_bbox(x, y, width, depth, min_z, max_z):
+def draw_3d_bbox(x1, x2, x3, x4, min_z, max_z):
     points = [
-    [x - width/2, y - depth/2, min_z], # id == min_z
-    [x + width/2, y - depth/2, min_z],
-    [x - width/2, y + depth/2, min_z],
-    [x + width/2, y + depth/2, min_z],
-    [x - width/2, y - depth/2, max_z],
-    [x + width/2, y - depth/2, max_z],
-    [x - width/2, y + depth/2, max_z],
-    [x + width/2, y + depth/2, max_z],
+    [x3[0], x3[1], min_z], # id == min_z
+    [x2[0], x2[1], min_z],
+    [x1[0], x1[1], min_z],
+    [x4[0], x4[1], min_z],
+    [x3[0], x3[1], max_z],
+    [x2[0], x2[1], max_z],
+    [x1[0], x1[1], max_z],
+    [x4[0], x4[1], max_z],
     ]
 
     lines = [
@@ -111,6 +184,35 @@ def draw_3d_bbox(x, y, width, depth, min_z, max_z):
     ]
 
     return points, np.array(lines)
+
+# def draw_3d_bbox(x, y, width, depth, min_z, max_z):
+#     points = [
+#     [x - width/2, y - depth/2, min_z], # id == min_z
+#     [x + width/2, y - depth/2, min_z],
+#     [x - width/2, y + depth/2, min_z],
+#     [x + width/2, y + depth/2, min_z],
+#     [x - width/2, y - depth/2, max_z],
+#     [x + width/2, y - depth/2, max_z],
+#     [x - width/2, y + depth/2, max_z],
+#     [x + width/2, y + depth/2, max_z],
+#     ]
+
+#     lines = [
+#         [0, 1],
+#         [0, 2],
+#         [1, 3],
+#         [2, 3],
+#         [4, 5],
+#         [4, 6],
+#         [5, 7],
+#         [6, 7],
+#         [0, 4],
+#         [1, 5],
+#         [2, 6],
+#         [3, 7],
+#     ]
+
+#     return points, np.array(lines)
 
 def draw_ground_line(arr_x, arr_y, arr_color):
 
